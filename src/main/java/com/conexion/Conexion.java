ad01be6b990d1e39f97d8f4419f93134afe1894a
Marv <marvin4alarcon3@gmail.com>
2019-03-10 11:50:58 -0600

    conexion java

---
+++ src/main/java/com/conexion/Conexion.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author User
+ */
+public class Conexion {
+     private static Connection con;
+    private static String stringConexion;
+    private static boolean connected = false;
+    
+    /**
+     * 
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException 
+     */
+    
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+         Class.forName("com.mysql.jdbc.Driver");
+        stringConexion = "jdbc:mysql://localhost/library?useSSL=FALSE&allowPublicKeyRetrieval=true";
+        con = DriverManager.getConnection(stringConexion,"root","1234");
+        connected = true; 
+        return con;
+    }
+    
+}
