7cce76c7c0364a29fed1f7d6f82f06d143713548
Marv <marvin4alarcon3@gmail.com>
2019-05-16 10:58:42 -0600

    Diseño framework bulma

--- src/main/java/com/conexion/Conexion.java
+++ src/main/java/com/conexion/Conexion.java
@@ -10,7 +10,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- *
+ * Clase Java, sirve para conectar la app web con la base de datos
  * @author User
  */
 public class Conexion {
